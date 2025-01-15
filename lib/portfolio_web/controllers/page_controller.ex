46ce7411cb36169ad2744fa4dfdac1a0c8bf53fc
jeonjiwan94 <jeonjiwan94@gamil.com>
2025-01-16 02:27:41 +0900

    reset main page

--- lib/portfolio_web/controllers/page_controller.ex
+++ lib/portfolio_web/controllers/page_controller.ex
@@ -2,8 +2,8 @@ defmodule PortfolioWeb.PageController do
   use PortfolioWeb, :controller
 
   def home(conn, _params) do
-    # The home page is often custom made,
-    # so skip the default app layout.
-    render(conn, :home, layout: false)
+    conn
+    |> assign(:csrf_token, Plug.CSRFProtection.get_csrf_token())
+    |> render("home.html")
   end
 end
